5d1e09ac4c5f093a8fd57da5e91d0e6e0c119261
WajahatAliAbid <Wajahatckl@outlook.com>
2019-07-26 00:26:52 +0500

    added add function

--- program.cpp
+++ program.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include "add.h"
 using namespace std;
 
 int main()
 {
     cout << "Hello World" << endl;
-    float number;
+    int number;
     number = 5;
+    number << add(number, 5);
+
+    cout << number << endl;
+
     return 0;
 }
