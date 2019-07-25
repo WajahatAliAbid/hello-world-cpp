660e07e03426e9c5e0ca48a475e9c70d6f4d69b3
WajahatAliAbid <Wajahatckl@outlook.com>
2019-07-25 23:45:58 +0500

    fixed launch and tasks.json

--- program.cpp
+++ program.cpp
@@ -3,12 +3,8 @@ using namespace std;
 
 int main()
 {
-    cout <<"Hello World"<<endl;
-    cout << GetNumber();
+    cout << "Hello World" << endl;
+    float number;
+    number = 5;
     return 0;
-}
-
-int GetNumber()
-{
-    return 5;
 }
