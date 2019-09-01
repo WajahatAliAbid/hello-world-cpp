6d751ddb7c9fe3067ecc21e97c2f3c8a32157e83
WajahatAliAbid <Wajahatckl@outlook.com>
2019-09-01 20:08:00 +0500

    added a class

--- program.cpp
+++ program.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 #include "functions.h"
 using namespace std;
+class Book
+{
+
+public:
+    string Name;
+    string Author;
+    int Pages;
+    Book(string name, string author, int pages)
+    {
+        cout << "Creating a book" << endl;
+        Name = name;
+        Author = author;
+        Pages = pages;
+    }
+};
+// using pointer functions
+void increment(int *number)
+{
+    *number = *number + 1;
+}
 
 int main()
 {
     cout << "Hello World" << endl;
-    int number;
-    number = 5;
+    int number = 5;
     number = add(number, 5);
 
+    cout << "The sum is " << number << endl;
+    increment(&number);
     cout << number << endl;
-
+    Book book1("Harry Potter", "JK Rowling", 250);
     return 0;
 }
